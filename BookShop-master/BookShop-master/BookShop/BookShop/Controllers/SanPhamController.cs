0320bd8168e637d887f4bedae9a30e2aff47c94d
Liêu Nguyên <nguyenlieu6369@gmail.com>
2024-12-13 12:38:56 +0700

    aa
    aa

---
+++ BookShop-master/BookShop-master/BookShop/BookShop/Controllers/SanPhamController.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq.Dynamic.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SlugGenerator;
+using BookShop.Models;
+namespace BookShop.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+        public async Task<IActionResult> Index()
+        {
+            // var iTShopDbContext = _context.SanPham.Include(s => s.HangSanXuat).Include(s => s.LoaiSanPham);
+            // return View(await iTShopDbContext.ToListAsync());
+
+            return View();
+        }
+
+        // POST: SanPham/Index_LoadData
+        [HttpPost]
+        public IActionResult Index_LoadData()
+        {
+            try
+            {
+                var draw = Request.Form["draw"].FirstOrDefault();
+                var start = Request.Form["start"].FirstOrDefault();
+                var length = Request.Form["length"].FirstOrDefault();
+                var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+                var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+                var searchValue = Request.Form["search[value]"].FirstOrDefault();
+
+                int pageSize = length != null ? int.Parse(length) : 0;
+                int skip = start != null ? int.Parse(start) : 0;
+                int totalRecords = 0;
+                int filterRecords = 0;
+
+                var sanPham = from r in _context.SanPham
+                              select new
+                              {
+                                  ID = r.ID,
+                                  HinhAnh = r.HinhAnh,
+                                  TenLoai = r.LoaiSanPham.TenLoai,
+                                  TenHangSanXuat = r.HangSanXuat.TenHangSanXuat,
+                                  TenSanPham = r.TenSanPham,
+                                  SoLuong = r.SoLuong,
+                                  DonGia = r.DonGia
+                              };
+
+                totalRecords = sanPham.Count();
+
+                // Sắp xếp
+                if (!string.IsNullOrEmpty(sortColumn) && !string.IsNullOrEmpty(sortColumnDirection))
+                {
+                    sanPham = sanPham.OrderBy(sortColumn + " " + sortColumnDirection);
+                }
+
+                // Tìm kiếm
+                if (!string.IsNullOrEmpty(searchValue) && !string.IsNullOrWhiteSpace(searchValue))
+                {
+                    sanPham = sanPham.Where(r => r.TenLoai.Contains(searchValue) ||
+                    r.TenHangSanXuat.Contains(searchValue) ||
+                   r.TenSanPham.Contains(searchValue) ||
+                   r.SoLuong.ToString().Contains(searchValue) ||
+                   r.DonGia.ToString().Contains(searchValue));
+                }
+
+                filterRecords = sanPham.Count();
+                var data = sanPham.Skip(skip).Take(pageSize).ToList();
+                var jsonData = new
+                {
+                    draw = draw,
+                    recordsFiltered = filterRecords,
+                    recordsTotal = totalRecords,
+                    data = data
+                };
+
+                return Json(jsonData, new System.Text.Json.JsonSerializerOptions());
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ BookShop-master/BookShop-master/BookShop/BookShop/Models/LoaiSanPham.cs
@@ -0,0 +1,22 @@
+﻿using BookShop.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace BookShop.Models
+{
+    public class LoaiSanPham
+    {
+        [DisplayName("Mã loại")]
+        public int ID { get; set; }
+
+        [StringLength(255)]
+        [Required(ErrorMessage = "Tên loại không được bỏ trống.")]
+        [DisplayName("Tên loại")]
+        public string TenLoai { get; set; }
+
+        [StringLength(255)]
+        [DisplayName("Tên loại không dấu")]
+        public string? TenLoaiKhongDau { get; set; }
+
+        public ICollection<SanPham>? SanPham { get; set; }
+    }
+}
