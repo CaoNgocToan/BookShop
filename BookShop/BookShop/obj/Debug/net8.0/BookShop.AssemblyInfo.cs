3d087eec40d988c05433ea5da4e46363065a7209
CaoNgocToan <121233032+CaoNgocToan@users.noreply.github.com>
2024-12-19 08:23:24 +0700

    uploadUI2

--- BookShop/BookShop/obj/Debug/net8.0/BookShop.AssemblyInfo.cs
+++ BookShop/BookShop/obj/Debug/net8.0/BookShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37fe0c0426d4ad78e8b51c21f6808d3a23cabe5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4c374c3a62fba438b7b6a28bca8137b50f3180")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
